5cdd6c8765b841bf6c6f3816b5e329ae07f7f6ce
migue <migue@DESKTOP-MGITEER>
2023-05-17 12:10:35 +0200

    Cambios en Generos y Cubiertas.

--- src/enums/Cubiertas.java
+++ src/enums/Cubiertas.java
@@ -2,7 +2,6 @@ package enums;
 
 public enum Cubiertas {
 
-
 	/**
 	 * Clase que gestiona el tipo de cubiertas de los CDs.
 	 */
--- src/enums/Generos.java
+++ src/enums/Generos.java
@@ -8,4 +8,5 @@ public enum Generos {
 	 */
 		POP, ROCK, METAL, JAZZ, HIPHOP;
 	
+	
 }
