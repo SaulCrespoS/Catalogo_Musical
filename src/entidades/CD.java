aa85b74543f280ad4fc2a7831a832fb3a24b74bf
migue <migue@DESKTOP-MGITEER>
2023-05-22 08:57:46 +0200

    Enums

--- src/entidades/CD.java
+++ src/entidades/CD.java
@@ -2,18 +2,26 @@ package entidades;
 
 import java.time.LocalDate;
 
+import enums.Cubiertas;
+import enums.Generos;
+
 public class CD extends Medio{
 
 	
-//	private enum Cubierta;
+	private Cubiertas cubierta;
+	private Generos genero;
 	private String[] pistas;
 	
-	public CD(){
+	public CD() {
 		super();
+		this.cubierta = Cubiertas.JEWEL;
+		this.genero = Generos.ROCK;
 		this.pistas=new String[10];;
 	}
-	public CD(String nombre, String artista, int duracion, LocalDate flanzamiento, String[] pistas) {
+	public CD(String nombre, String artista, int duracion, LocalDate flanzamiento, Cubiertas cubierta, Generos genero, String[] pistas) {
 		super(nombre,artista, duracion, flanzamiento);
+		this.cubierta = cubierta;
+		this.genero = genero;
 		this.pistas=pistas;
 	}
 	public String[] getPistas() {
